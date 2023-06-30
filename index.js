0bd07c350704e848b8e04b95ad9bfebb55e056e5
Anurag Srivastava <anuanurag2410@gmail.com>
2023-06-30 20:50:38 +0530

    Added Partials and Layouts

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app=express();
 const port = 8000;
 const path=require('path');
 
+const expressLayouts=require('express-ejs-layouts');
+
+
 //use express router
 app.use('/',require('./routes/index'));
 
