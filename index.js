a27665ea209c9df7a85ff11a08e239500e02efa1
Anurag Srivastava <anuanurag2410@gmail.com>
2023-07-06 22:07:09 +0530

    Sign up and Signin Added

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 const port = 8000;
-const path=require('path');
-
-const expressLayouts=require('express-ejs-layouts');
-
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 app.use(express.static('./assets'));
+
 app.use(expressLayouts);
-//use express router
-app.use('/',require('./routes/index'));
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
-//Setup of View Engine
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,'views'));
 
-//Firing the Express Server
-app.listen(port,function(err){
-if (err){
-    console.log('Error in running the Server ',err);
-}
-console.log('YUP!!The Express Server is Running on Port:',port);
-}); 
+// use express router
+app.use('/', require('./routes'));
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
-const express=require('express');
+const express = require('express');
 
-const router=express.Router();
-const homeController=require('../controller/home_controller');
+const router = express.Router();
+const homeController = require('../controllers/home_controller');
 
-console.log('Router Loaded!!');
+console.log('router loaded');
 
-router.get('/',homeController.home);
 
-router.use('/users',require('./users'));
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
 
-router.use('/posts',require('./posts'));
-module.exports=router;
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
+
+
+module.exports = router;
