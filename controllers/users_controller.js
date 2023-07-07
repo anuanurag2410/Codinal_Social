07d22c732ac5275265c346d6e5fc37fa46aa4171
Anurag Srivastava <anuanurag2410@gmail.com>
2023-07-07 09:58:34 +0530

    Signup authentication done manually

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User=require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'User Profile'
@@ -23,7 +25,33 @@ module.exports.signIn = function(req, res){
 
 //get the signup data 
 module.exports.create=function(req,res){
-    //Todo
+    if (req.body.password!=req.body.confirm_password)
+    {
+        return res.redirect('back');
+    }    
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){console.log('Error in Finding User in Signup!');
+        return
+    }
+
+    if (!user){
+        user.create(req.body,function(err,user){
+            if(err){console.log('Error in Creating user while Signing UP!');
+        return 
+    }
+
+        return res.redirect('/users/sign-in')
+
+        })
+    }
+
+        else{
+            return res.redirect('back');
+        }
+    
+
+    });
+
 }
 
 //Signin and Create Session for the User
